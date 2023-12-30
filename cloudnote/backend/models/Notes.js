9006cfe50793df6031e8d85195ef103934413eb4
Prince <princejaj67@gmail.com>
2023-12-31 00:40:10 +0530

    Updating cloudnote project

---
+++ cloudnote/backend/models/Notes.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const NotesSchema = new Schema({
+    name :{
+        type : String,
+        required: true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    description : {
+        type:String,
+    },
+    tag :{
+        type : String,
+    },
+    date : {
+        type : Date,
+        Default : Date.now
+    }
+})
+
+module.exports = mongoose.model('user',NotesSchema);
