aa6f431b86d877eac5e81ef3595ba2798dc3ace2
Prince <princejaj67@gmail.com>
2024-01-13 15:51:23 +0530

    Adding express validator

--- cloudnote/backend/routes/auth.js
+++ cloudnote/backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
-const {query, validationResult} = require('express-validator')
+const {body, validationResult} = require('express-validator')
 // router.get('/',(req,res)=>{
 //     const a = {
 //         str : "hiiiiiioioioioioio",
@@ -10,34 +10,24 @@ const {query, validationResult} = require('express-validator')
 //     // res.json(a);
 //     res.send(req.body)
 // })
-/* router.post('/',[query('name').isLength({min : 3})],(req,res)=>{
+
+
+router.post('/', 
+[ 
+    body('name').notEmpty(),
+    body('email').isEmail(),
+    body('password').isAlphanumeric()
+], 
+(req, res) => {
     const result = validationResult(req);
-    console.log(result)
-    if (!result.isEmpty()) {
+    if (result.isEmpty()) {
         const u = User(req.body);
         u.save();
-        return res.send(`Hello, ${req.query.name}!`);
+        return res.send(`Hello, ${req.body.name}!, your schema is added`);
     }
-    
-    res.send({ errors: result.array() });
-    // const jsonResult = validationResult(res);
-    // console.log(jsonResult.isEmpty())
-
-    // if(!jsonResult.isEmpty()){
-    //     return res.status(200).json({errors : errors.Array()});
-    // }
-
-    // res.send("Added body data to database")
-}) */
-
-router.post('/', query('name').notEmpty(), (req, res) => {
-    const result = validationResult(req);
-    console.log(query('name').notEmpty().toBoolean)
-    // if (!result.isEmpty()) {
-    //   return res.send(`Hello, ${req.query.name}!`);
-    // }
-  
-    res.send({ errors: result.array() });
+    else {
+        res.send({ errors: result.array() });
+    }
   });
 
 module.exports = router;
