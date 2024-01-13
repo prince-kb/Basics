7cd899a23cb712c7e9e222682924cc20c9d0dd23
Prince <princejaj67@gmail.com>
2024-01-13 17:35:10 +0530

    User signup usinasync await

--- cloudnote/backend/routes/auth.js
+++ cloudnote/backend/routes/auth.js
@@ -18,28 +18,34 @@ router.post('/',
     body('email').isEmail(),
     body('password','Password length must be greater than 4').isLength({min : 4})
 ], 
-(req, res) => {
+async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
         res.send({ errors: result.array() });
     }
-    else {
      /* const u = User(req.body);
         u.save();
         return res.send(`Hello, ${req.body.name}!, your schema is added`); */
         //OR
-        User.create({
+        let user = await User.findOne({email : req.body.email})
+        if(user){
+            // console.log(user)
+            return res.status(400).json({error : "Email already exist",message : "Try using another email"})
+        }
+        user = User.create({
             name : req.body.name,
             email : req.body.email,
             password : req.body.password,
             description : req.body.description
-        }).then(user=>{res.json(user)
+        })
+        res.send("Added user : " + req.body.name)
+        
+/*         .then(user=>{res.json(user)
             console.log("User added")
         })
         .catch(err=>{console.log(err)
         res.json({error : 'Enter unique attributes', message : err.message });
-    })
-    }
+    }) */
   });
 
 module.exports = router;
