c17e84eb027c771fd7301e21e41deb3a1b5f6374
Prince <princejaj67@gmail.com>
2024-01-14 17:02:27 +0530

    Added comments

--- cloudnote/backend/index.js
+++ cloudnote/backend/index.js
@@ -6,9 +6,13 @@ const port = 5000
 
 app.use(express.json())
 
+//res means response, req eans request
+
 app.get('/', (req, res) => {
   res.send('Hello World1!')
 })
+//This line helps the app to not full depened on the current file and use another file to simplify
+//So instead of get request , we areu= using use request
 // app.use('/auth',require('./routes/auth'));
 app.use('/auth/signup',require('./routes/auth'));
 
--- cloudnote/backend/routes/auth.js
+++ cloudnote/backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+//It should b einstalled seperately
 const {body, validationResult} = require('express-validator')
 // router.get('/',(req,res)=>{
 //     const a = {
@@ -11,7 +12,7 @@ const {body, validationResult} = require('express-validator')
 //     res.send(req.body)
 // })
 
-
+//To add data (here we are adding login information) 'post' is used majorly
 router.post('/', 
 [ 
     body('name').notEmpty(),
@@ -23,23 +24,36 @@ async (req, res) => {
     if (!result.isEmpty()) {
         res.send({ errors: result.array() });
     }
-     /* const u = User(req.body);
+        //User is a 
+
+        /* const u = User(req.body);
         u.save();
-        return res.send(`Hello, ${req.body.name}!, your schema is added`); */
+        return res.send(`Hello, ${req.body.name}!, your schema is added`); 
+        */
         //OR
+        try{
+            //This line should must be declared await
         let user = await User.findOne({email : req.body.email})
         if(user){
-            // console.log(user)
             return res.status(400).json({error : "Email already exist",message : "Try using another email"})
         }
+        //user is a variable and it will be send as response to save the data
         user = User.create({
             name : req.body.name,
             email : req.body.email,
             password : req.body.password,
             description : req.body.description
         })
+        //This line will return the user as response
+        console.log(req.body.name + " added")
         res.send("Added user : " + req.body.name)
         
+    }
+    catch(error){
+        console.log(error)
+        return res.status(500).json({error : "Some error occured from our side",message : "We are trying to fix it"})
+    }
+        //We can also use .then and .catch if we are not using an await request
 /*         .then(user=>{res.json(user)
             console.log("User added")
         })
