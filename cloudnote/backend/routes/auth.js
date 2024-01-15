505cfee6469089287e7ea4c062890f857341c53c
Prince <princejaj67@gmail.com>
2024-01-15 09:48:41 +0530

    Added bcryptjs

--- cloudnote/backend/routes/auth.js
+++ cloudnote/backend/routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const User = require('../models/User')
 //It should b einstalled seperately
 const {body, validationResult} = require('express-validator')
+
+//To encrypt a password we use the following package
+const bcryptjs = require('bcryptjs')
 // router.get('/',(req,res)=>{
 //     const a = {
 //         str : "hiiiiiioioioioioio",
@@ -11,6 +14,11 @@ const {body, validationResult} = require('express-validator')
 //     // res.json(a);
 //     res.send(req.body)
 // })
+
+//Using jsonwebtoken to simplify login process and not dealing with security
+//It basically generates a token for signing in and has three parts - Header,payload and signature
+var jwt = require('jsonwebtoken');
+var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 
 //To add data (here we are adding login information) 'post' is used majorly
 router.post('/', 
@@ -24,7 +32,13 @@ async (req, res) => {
     if (!result.isEmpty()) {
         res.send({ errors: result.array() });
     }
-        //User is a 
+
+        //Both bcryptjs lines return a promise, so we need to add await
+        //Salt will add additional strings to be generated to the hash table
+        const salt = await bcryptjs.genSalt(10);
+        //This will generate a hash table for my password so that if my database is stolen, password cannot be retrieved or cannot be matched using a rainbow table
+        const securePassword = await bcryptjs.hash(req.body.password,salt);
+        //User is a schema defined in another js file
 
         /* const u = User(req.body);
         u.save();
@@ -41,7 +55,7 @@ async (req, res) => {
         user = User.create({
             name : req.body.name,
             email : req.body.email,
-            password : req.body.password,
+            password : securePassword,
             description : req.body.description
         })
         //This line will return the user as response
