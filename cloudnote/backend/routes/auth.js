3fc8fb03ad4ee524da735462130b5d4eeec09a9a
Prince <princejaj67@gmail.com>
2024-01-19 11:16:50 +0530

    Added comments and adding middleware next

--- cloudnote/backend/routes/auth.js
+++ cloudnote/backend/routes/auth.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 //It should b einstalled seperately
-const {body, validationResult} = require('express-validator')
+const { body, validationResult } = require("express-validator");
 
 //To encrypt a password we use the following package
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require("bcryptjs");
 // router.get('/',(req,res)=>{
 //     const a = {
 //         str : "hiiiiiioioioioioio",
@@ -17,110 +17,127 @@ const bcryptjs = require('bcryptjs')
 
 //Using jsonwebtoken to simplify login process and not dealing with security
 //It basically generates a token for signing in and has three parts - Header,payload and signature
-var jwt = require('jsonwebtoken');
-const jwwwtoken = "hiiieyo"
+var jwt = require("jsonwebtoken");
+const jwwwtoken = "hiiieyo";
 
 //To add data (here we are adding login information) 'post' is used majorly
-router.post('/signup', 
-[ 
-    body('name').notEmpty(),
-    body('email').isEmail(),
-    body('password','Password length must be greater than 4').isLength({min : 4})
-], 
-async (req, res) => {
+//First route : To signup user
+//Uses express-validator to check password and data and bcryptjs to save password to be unavailable directly
+router.post(
+  "/signup",
+  [
+    body("name").notEmpty(),
+    body("email").isEmail(),
+    body("password", "Password length must be greater than 4").isLength({
+      min: 4,
+    }),
+  ],
+  async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
-        res.send({ errors: result.array() });
+      res.send({ errors: result.array() });
     }
 
-        //Both bcryptjs lines return a promise, so we need to add await
-        //Salt will add additional strings to be generated to the hash table
-        const salt = await bcryptjs.genSalt(10);
-        //This will generate a hash table for my password so that if my database is stolen, password cannot be retrieved or cannot be matched using a rainbow table
-        const securePassword = await bcryptjs.hash(req.body.password,salt);
-        //User is a schema defined in another js file
+    //Both bcryptjs lines return a promise, so we need to add await
+    //Salt will add additional strings to be generated to the hash table
+    const salt = await bcryptjs.genSalt(10);
+    //This will generate a hash table for my password so that if my database is stolen, password cannot be retrieved or cannot be matched using a rainbow table
+    const securePassword = await bcryptjs.hash(req.body.password, salt);
+    //User is a schema defined in another js file
 
-        /* const u = User(req.body);
+    /* const u = User(req.body);
         u.save();
         return res.send(`Hello, ${req.body.name}!, your schema is added`); 
         */
-        //OR
-        try{
-            //This line should must be declared await
-        let user = await User.findOne({email : req.body.email})
-        if(user){
-            //We cannot tell users that this email already exist as this will help attackers in some way
-            //Instead we will just response by some error occured
-            return res.status(400).json({error : "Invalid credentials",message : "Try using another email"})
-        }
-        //user is a variable and it will be send as response to save the data
-        user = User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : securePassword,
-            description : req.body.description
-        })
-        const data = {
-            user:{
-                id : user.id
-            }
-        }
-        const authToken = jwt.sign(data,jwwwtoken)
-        console.log(req.body.name + " added")
+    //OR
+    try {
+      //This line should must be declared await
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        //We cannot tell users that this email already exist as this will help attackers in some way
+        //Instead we will just response by some error occured
+        return res
+          .status(400)
+          .json({
+            error: "Invalid credentials",
+            message: "Try using another email",
+          });
+      }
+      //user is a variable and it will be send as response to save the data
+      user = User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: securePassword,
+        description: req.body.description,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, jwwwtoken);
+      console.log(req.body.name + " added");
 
-
-        //This line will return the user as response
-        // res.send("Added user : " + req.body.name)
-        res.json(authToken)
-        
+      //This line will return the user as response
+      // res.send("Added user : " + req.body.name)
+      res.json(authToken);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({
+          error: "Some error occured from our side",
+          message: "We are trying to fix it",
+        });
     }
-    catch(error){
-        console.log(error)
-        return res.status(500).json({error : "Some error occured from our side",message : "We are trying to fix it"})
-    }
-        //We can also use .then and .catch if we are not using an await request
-/*         .then(user=>{res.json(user)
+    //We can also use .then and .catch if we are not using an await request
+    /*         .then(user=>{res.json(user)
             console.log("User added")
         })
         .catch(err=>{console.log(err)
         res.json({error : 'Enter unique attributes', message : err.message });
     }) */
-  });
+  }
+);
 
-router.post('/login', 
-[ 
-    body('email').isEmail(),
-    body('password').exists()
-], 
-async (req, res) => {
+//Second Route : Login user requires bcrypt js to check password and returns jsonwebtokens
+router.post(
+  "/login",
+  [body("email").isEmail(), body("password").exists()],
+  async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
-        res.send({ errors: result.array() });
+      res.send({ errors: "Invalid email or password" });
     }
-        const {email,password}= req.body;
-        try{
-        let user = await User.findOne({email})
-        if(!user){
-            return res.status(400).json({error : "Invalid credentials"});
-        }
-        const passComparison = bcryptjs.compare(password,user.password);
-        if(!passComparison){
-            return res.status(400).json({error : "Invalid credentials"});
-        }
-
-        const data = {
-            user:{
-                id : user.id
-            }
-        }
-        const authToken = jwt.sign(data,jwwwtoken)
-        res.json(authToken)
-        
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Invalid credentials" });
+      }
+      //Comparison password provided during login and password stored using hash table
+      const passComparison = bcryptjs.compare(password, user.password);
+      if (!passComparison) {
+        return res.status(400).json({ error: "Invalid credentials" });
+      }
+      //Returning json token as response for now
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, jwwwtoken);
+      res.json(authToken);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({
+          error: "Some error occured from our side",
+          message: "We are trying to fix it",
+        });
     }
-    catch(error){
-        console.log(error)
-        return res.status(500).json({error : "Some error occured from our side",message : "We are trying to fix it"})
-    }
-  });
+  }
+);
 
 module.exports = router;
