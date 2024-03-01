3008af085d87d074c804f0fddd574c2bcbb7ca3e
Prince <princejaj67@gmail.com>
2024-03-01 14:01:15 +0530

    deleted unmaintained apps

--- practice/src/Components/ColorComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ColorComponent(props) {
-  return (
-    <div style={{height : '2vw',width: '2vw',backgroundColor : `${props.color}`,display : 'inline-block',margin :'2px',transition : '1s all ease',border : `0.4vh dashed black`}}>
-      
-    </div>
-  )
-}
